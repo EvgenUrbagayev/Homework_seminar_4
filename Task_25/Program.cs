3e168e36ebe819be33d65c7cafa84d460de262eb
Evgeniy <eurbagayev@mail.com>
2022-10-04 22:34:09 +0300

    Возводит А в степень В

--- Task_25/Program.cs
+++ Task_25/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 25: Напишите метод, который принимает на вход два числа
+// (A и B) и возводит число A в натуральную степень B с использованием цикла.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+using static System.Console;
+Clear();
+
+WriteLine("Введите число:  ");
+int A = Convert.ToInt32(Console.ReadLine());
+WriteLine("Введите число:  ");
+int B = Convert.ToInt32(Console.ReadLine());
+int exponent = A;
+
+for (int i = 1; i < B; i++)
+{
+exponent = exponent * A;
+}
+WriteLine($"A в степени B равно: {exponent}");
