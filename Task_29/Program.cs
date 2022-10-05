2319dc56af8dbb7071354589430e06498d722725
Evgeniy <eurbagayev@mail.com>
2022-10-05 21:39:37 +0300

    Формирование массива из 8 случайных чисел

---
+++ Task_29/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 29: Напишите метод, который формирует массив
+// из 8 случайных элементов и выводит их на экран.
+
+using static System.Console;
+Clear();
+int[] myArray = new int[8];
+{
+    for(int i=0; i < 8; i++)
+    {
+        myArray[i] = new Random().Next(0, 34);
+        Write($"{myArray[i]}, ");
+    }
+}
+WriteLine();
+
+
+
